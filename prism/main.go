c28837f216b71f8c61f97ccc2b035bdd9bdd4b0a
Jesse Szwedko <j.szwedko@modcloth.com>
2013-11-08 14:04:59 -0500

    Consuming and inserting messages into database

--- prism/main.go
+++ prism/main.go
@@ -7,11 +7,13 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	_ "code.google.com/p/gosqlite/sqlite3"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	"github.com/modcloth-labs/schema_ensurer"
+	"github.com/nu7hatch/gouuid"
 	"github.com/streadway/amqp"
 )
 
@@ -19,7 +21,7 @@ var (
 	migrations = map[string][]string{
 		"20131108000000": {`
 	  CREATE TABLE IF NOT EXISTS messages(
-		id serial PRIMARY KEY,
+		uuid char(32),
 
     content_type character varying(256),
     content_encoding character varying(256),
@@ -32,6 +34,9 @@ var (
     type character varying(256),
     user_id character varying(256),
 
+    exchange character varying(256),
+    routing_key character varying(256),
+
 		body text,
 
 		created_at timestamp without time zone NOT NULL
@@ -39,11 +44,32 @@ var (
 	  `,
 		},
 	}
+	insertSql = `
+      INSERT INTO messages(
+        uuid,
+        content_type,
+        content_encoding,
+        delivery_mode,
+        priority,
+        correlation_id,
+        reply_to,
+        expiration,
+        timestamp,
+        type,
+        user_id,
+        exchange,
+        routing_key,
+        body,
+        created_at
+      ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `
+
 	logger = log.New(os.Stderr, "[prism] ", log.LstdFlags)
 
 	databaseDriverFlag string
 	databaseUriFlag    string
 	amqpUriFlag        string
+	queueNameFlag      string
 )
 
 func init() {
@@ -57,28 +83,39 @@ func init() {
 	flag.StringVar(&databaseDriverFlag, "database-driver", "sqlite3", "Database driver to use (possible values: sqlite3, mysql, postgres)")
 	flag.StringVar(&databaseUriFlag, "database-uri", "messages.db", "Database uri")
 	flag.StringVar(&amqpUriFlag, "amqp-uri", "amqp://guest:guest@localhost:5672/", "AMQP connection URI")
+	flag.StringVar(&queueNameFlag, "queue", "default", "Queue to consume from")
 	flag.Parse()
 }
 
 func main() {
 	var (
-		db             *sql.DB
-		schemaEnsurer  *sensurer.SchemaEnsurer
-		amqpConnection *amqp.Connection
-		amqpChannel    *amqp.Channel
-		err            error
+		db              *sql.DB
+		u4              *uuid.UUID
+		schemaEnsurer   *sensurer.SchemaEnsurer
+		amqpConnection  *amqp.Connection
+		amqpChannel     *amqp.Channel
+		deliveries      <-chan (amqp.Delivery)
+		insertStatement *sql.Stmt
+		err             error
 	)
 
 	if db, err = sql.Open(databaseDriverFlag, databaseUriFlag); err != nil {
 		log.Printf("Could not connect to database: %s", err)
 		os.Exit(1)
 	}
+	defer db.Close()
 
 	schemaEnsurer = sensurer.New(db, migrations, logger)
 	if err = schemaEnsurer.EnsureSchema(); err != nil {
 		log.Printf("Could not create schema: %s", err)
 		os.Exit(1)
 	}
+
+	if insertStatement, err = db.Prepare(insertSql); err != nil {
+		log.Printf("Could not prepare insert statement: %s", err)
+		os.Exit(1)
+	}
+	defer insertStatement.Close()
 
 	if amqpConnection, err = amqp.Dial(amqpUriFlag); err != nil {
 		log.Printf("Could not connect to RabbitMQ: %s", err)
@@ -91,5 +128,44 @@ func main() {
 		os.Exit(1)
 	}
 
-  _ = amqpChannel
+	if deliveries, err = amqpChannel.Consume(queueNameFlag, "", false, false, false, false, nil); err != nil {
+		log.Printf("Could not consume from RabbitMQ: %s", err)
+		os.Exit(1)
+	}
+
+	for delivery := range deliveries {
+		log.Printf("Consuming %+v", delivery)
+
+		if u4, err = uuid.NewV4(); err != nil {
+			log.Printf("Could not create GUID: %s", err)
+			os.Exit(1)
+		}
+
+		if _, err = insertStatement.Exec(
+			u4.String(),
+			delivery.ContentType,
+			delivery.ContentEncoding,
+			delivery.DeliveryMode,
+			delivery.Priority,
+			delivery.CorrelationId,
+			delivery.ReplyTo,
+			delivery.Expiration,
+			delivery.Timestamp,
+			delivery.Type,
+			delivery.UserId,
+			delivery.Exchange,
+			delivery.RoutingKey,
+			delivery.Body,
+			time.Now()); err != nil {
+			log.Printf("Failed to consume: %s", err)
+			if err = delivery.Nack(false, true); err != nil {
+				log.Printf("Failed to nack: %s", err)
+			}
+			continue
+		}
+
+		if err = delivery.Ack(false); err != nil {
+			log.Printf("Failed to ack: %s", err)
+		}
+	}
 }
