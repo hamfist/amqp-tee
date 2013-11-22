8937eb410ce68de0004d4f328e9e7531f91bbd99
Jesse Szwedko <j.szwedko@modcloth.com>
2013-11-22 17:28:45 -0500

    Pulling out storage of deliveries to collaborator

---
+++ delivery_store.go
@@ -0,0 +1,120 @@
+package prism
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"log"
+	"time"
+
+	_ "code.google.com/p/gosqlite/sqlite3"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	"github.com/modcloth-labs/schema_ensurer"
+	"github.com/nu7hatch/gouuid"
+	"github.com/streadway/amqp"
+)
+
+var (
+	migrations = map[string][]string{
+		"20131108000000": {`
+	  CREATE TABLE IF NOT EXISTS deliverys(
+		uuid char(32),
+
+    content_type character varying(256),
+    content_encoding character varying(256),
+    delivery_mode smallint,
+    priority smallint,
+    correlation_id character varying(256),
+    reply_to character varying(256),
+    expiration character varying(256),
+    timestamp timestamp with time zone,
+    type character varying(256),
+    user_id character varying(256),
+
+    exchange character varying(256),
+    routing_key character varying(256),
+
+		body text,
+
+		created_at timestamp without time zone NOT NULL
+	  );
+	  `,
+		},
+	}
+	insertSql = `
+      INSERT INTO deliverys(
+        uuid,
+        content_type,
+        content_encoding,
+        delivery_mode,
+        priority,
+        correlation_id,
+        reply_to,
+        expiration,
+        timestamp,
+        type,
+        user_id,
+        exchange,
+        routing_key,
+        body,
+        created_at
+      ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `
+)
+
+type DeliveryStore struct {
+	db              *sql.DB
+	insertStatement *sql.Stmt
+}
+
+func NewDeliveryStore(databaseDriver string, databaseUri string) (deliveryStore *DeliveryStore, err error) {
+	me = &DeliveryStore{}
+
+	if me.db, err = sql.Open(databaseDriver, databaseUri); err != nil {
+		return nil, err
+	}
+
+	schemaEnsurer := sensurer.New(me.db, migrations, log.New(ioutil.Discard, "", 0))
+	if err = schemaEnsurer.EnsureSchema(); err != nil {
+		return nil, err
+	}
+
+	if me.insertStatement, err = me.db.Prepare(insertSql); err != nil {
+		return nil, err
+	}
+
+	return me, nil
+}
+
+func (me *DeliveryStore) Store(delivery *amqp.Delivery) (err error) {
+	var (
+		u4 *uuid.UUID
+	)
+
+	if u4, err = uuid.NewV4(); err != nil {
+		return err
+	}
+
+	_, err = me.insertStatement.Exec(
+		u4.String(),
+		delivery.ContentType,
+		delivery.ContentEncoding,
+		delivery.DeliveryMode,
+		delivery.Priority,
+		delivery.CorrelationId,
+		delivery.ReplyTo,
+		delivery.Expiration,
+		delivery.Timestamp,
+		delivery.Type,
+		delivery.UserId,
+		delivery.Exchange,
+		delivery.RoutingKey,
+		delivery.Body,
+		time.Now())
+
+	return err
+}
+
+func (me *DeliveryStore) Close() {
+	me.db.Close()
+}
--- prism/main.go
+++ prism/main.go
@@ -1,69 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"time"
 
-	_ "code.google.com/p/gosqlite/sqlite3"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
-	"github.com/modcloth-labs/schema_ensurer"
-	"github.com/nu7hatch/gouuid"
+	"github.com/modcloth-labs/prism"
 	"github.com/streadway/amqp"
 )
 
 var (
-	migrations = map[string][]string{
-		"20131108000000": {`
-	  CREATE TABLE IF NOT EXISTS messages(
-		uuid char(32),
-
-    content_type character varying(256),
-    content_encoding character varying(256),
-    delivery_mode smallint,
-    priority smallint,
-    correlation_id character varying(256),
-    reply_to character varying(256),
-    expiration character varying(256),
-    timestamp timestamp with time zone,
-    type character varying(256),
-    user_id character varying(256),
-
-    exchange character varying(256),
-    routing_key character varying(256),
-
-		body text,
-
-		created_at timestamp without time zone NOT NULL
-	  );
-	  `,
-		},
-	}
-	insertSql = `
-      INSERT INTO messages(
-        uuid,
-        content_type,
-        content_encoding,
-        delivery_mode,
-        priority,
-        correlation_id,
-        reply_to,
-        expiration,
-        timestamp,
-        type,
-        user_id,
-        exchange,
-        routing_key,
-        body,
-        created_at
-      ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `
-
 	logger = log.New(os.Stderr, "[prism] ", log.LstdFlags)
 
 	databaseDriverFlag string
@@ -89,33 +37,19 @@ func init() {
 
 func main() {
 	var (
-		db              *sql.DB
-		u4              *uuid.UUID
-		schemaEnsurer   *sensurer.SchemaEnsurer
-		amqpConnection  *amqp.Connection
-		amqpChannel     *amqp.Channel
-		deliveries      <-chan (amqp.Delivery)
-		insertStatement *sql.Stmt
-		err             error
+		amqpConnection *amqp.Connection
+		amqpChannel    *amqp.Channel
+		deliveryStore  *prism.DeliveryStore
+		deliveries     <-chan (amqp.Delivery)
+		err            error
 	)
 
-	if db, err = sql.Open(databaseDriverFlag, databaseUriFlag); err != nil {
-		log.Printf("Could not connect to database: %s", err)
-		os.Exit(1)
-	}
-	defer db.Close()
-
-	schemaEnsurer = sensurer.New(db, migrations, logger)
-	if err = schemaEnsurer.EnsureSchema(); err != nil {
-		log.Printf("Could not create schema: %s", err)
+	if deliveryStore, err = prism.NewDeliveryStore(databaseDriverFlag, databaseUriFlag); err != nil {
+		log.Printf("Could not create message store: %s", err)
 		os.Exit(1)
 	}
 
-	if insertStatement, err = db.Prepare(insertSql); err != nil {
-		log.Printf("Could not prepare insert statement: %s", err)
-		os.Exit(1)
-	}
-	defer insertStatement.Close()
+	defer deliveryStore.Close()
 
 	if amqpConnection, err = amqp.Dial(amqpUriFlag); err != nil {
 		log.Printf("Could not connect to RabbitMQ: %s", err)
@@ -136,27 +70,7 @@ func main() {
 	for delivery := range deliveries {
 		log.Printf("Consuming %+v", delivery)
 
-		if u4, err = uuid.NewV4(); err != nil {
-			log.Printf("Could not create GUID: %s", err)
-			os.Exit(1)
-		}
-
-		if _, err = insertStatement.Exec(
-			u4.String(),
-			delivery.ContentType,
-			delivery.ContentEncoding,
-			delivery.DeliveryMode,
-			delivery.Priority,
-			delivery.CorrelationId,
-			delivery.ReplyTo,
-			delivery.Expiration,
-			delivery.Timestamp,
-			delivery.Type,
-			delivery.UserId,
-			delivery.Exchange,
-			delivery.RoutingKey,
-			delivery.Body,
-			time.Now()); err != nil {
+		if err = deliveryStore.Store(&delivery); err != nil {
 			log.Printf("Failed to consume: %s", err)
 			if err = delivery.Nack(false, true); err != nil {
 				log.Printf("Failed to nack: %s", err)
