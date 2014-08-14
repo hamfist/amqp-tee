d76ef8890b4ee9828239d4109988ace5d144bb80
jszwedko <jesse.szwedko@gmail.com>
2014-08-14 15:16:37 -0400

    Install godep and golint in vagrant

--- .vagrant-provision-as-vagrant.sh
+++ .vagrant-provision-as-vagrant.sh
@@ -2,13 +2,16 @@
 set -x
 set -e
 
+mkdir -p ~/bin
+
 ln -svf /gopath/src/github.com/modcloth-labs/amqp-tee ~/
 ln -svf /vagrant/.vagrant-skel/bashrc /home/vagrant/.bashrc
 ln -svf /vagrant/.vagrant-skel/profile /home/vagrant/.profile
 
 source ~/.profile
 
-mkdir -p ~/bin
-
 wget http://guest:guest@localhost:55672/cli/rabbitmqadmin -O ~/bin/rabbitmqadmin
 chmod +x ~/bin/rabbitmqadmin
+
+go get github.com/kr/godep
+go get github.com/golang/lint/golint
