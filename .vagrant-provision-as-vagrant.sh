9969d7a198e55b62bc1264c150140cc3c6689a62
jszwedko <jesse.szwedko@gmail.com>
2014-08-14 15:08:37 -0400

    Symlink ~/amqp-tee to /gopath/src/github.com/modcloth-labs/amqp-tee

--- .vagrant-provision-as-vagrant.sh
+++ .vagrant-provision-as-vagrant.sh
@@ -2,6 +2,7 @@
 set -x
 set -e
 
+ln -svf /gopath/src/github.com/modcloth-labs/amqp-tee ~/amqp-tee
 ln -svf /vagrant/.vagrant-skel/bashrc /home/vagrant/.bashrc
 ln -svf /vagrant/.vagrant-skel/profile /home/vagrant/.profile
 
@@ -10,5 +11,4 @@ source ~/.profile
 mkdir -p ~/bin
 
 wget http://guest:guest@localhost:55672/cli/rabbitmqadmin -O ~/bin/rabbitmqadmin
-
 chmod +x ~/bin/rabbitmqadmin
